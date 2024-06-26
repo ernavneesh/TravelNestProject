1dd7041f70afe25663eab67012bea56847a2bbd6
ernavneesh <163695042+ernavneesh@users.noreply.github.com>
2024-06-26 17:00:59 -0400

    Update package.model.js

--- server/models/package.model.js
+++ server/models/package.model.js
@@ -1,42 +1,20 @@
 const mongoose = require('mongoose');
 
 const PackageSchema = mongoose.Schema({
-    destinationId: {
-        type: mongoose.Types.ObjectId,
-        ref: 'destination',
-        required: true,
-    },
-    packageName: {
-        type:String,
-        required: true,
-        trim: true
-    },
-    noOfDays: {
-        type:Number,
-        required: true
-    },
-    overviewDetails: {
-        type:String,
-        required: true
-    },
-    packageImage: {
-        type:String,
-        required: true
-    },
-    amountPerPerson: {
-        type:String,
-        required: true
-    },
-    locations: {
-
-    },
+    destinationId: {type: mongoose.Types.ObjectId, ref: 'destination', required: true,},
+    packageName: {type:String, required: true, trim: true},
+    noOfDays: {type:Number, required: true},
+    overviewDetails: {type:String, required: true},
+    packageImage: {type:String, required: true},
+    amountPerPerson: {type:String, required: true},
+    locations: {type:String, required: true},
     highlights: { type: [String], default: [] },
     itinerary: [
         {
-            shortDescription: String,
-            longDescription : String,
+            shortDescription: {type:String, required: true},
+            longDescription : {type:String, required: true},
             meal: ["Breakfast","Lunch","Dinner","Not Included"],
-            image: String
+            image: {type:String, required: true}
         }
     ]  
 }, {
