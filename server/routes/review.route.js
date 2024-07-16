023cb0e1b4537430393ba21cf6f56a47c3460c46
ernavneesh <163695042+ernavneesh@users.noreply.github.com>
2024-07-15 20:01:10 -0400

    Get reviews based on package id

--- server/routes/review.route.js
+++ server/routes/review.route.js
@@ -4,5 +4,6 @@ const Review = require('../controllers/review.controller');
 const verifyToken = require('../middleware/auth');
 
 router.post('/',verifyToken, Review.addReview);
+router.post('/:packageId', verifyToken, Review.getReviewsByPackage);
 
 module.exports = router;
