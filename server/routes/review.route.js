d8e273928937294655e4b62a8fd806df9de6d8ed
LakhvirDeol <lakhvirkaur366@gmail.com>
2024-07-23 18:54:24 -0500

    "Added review details"

--- server/routes/review.route.js
+++ server/routes/review.route.js
@@ -4,7 +4,7 @@ const Review = require('../controllers/review.controller');
 const verifyToken = require('../middleware/auth');
 
 router.post('/',verifyToken, Review.addReview);
-router.post('/:packageId', verifyToken, Review.getReviewsByPackage);
+router.get('/packageid/:packageId', verifyToken, Review.getReviewsByPackage);
 router.get('/:bookingId', verifyToken, Review.getReviewByBookingId);
 
 module.exports = router;
